6ca048fb4c93646cdd65054b358f7abf44b3c467
Fedor Andrianov <58761096+fedandr@users.noreply.github.com>
2026-08-26 21:26:47 +0300

    Update sicp1-09.go

--- sicp1/sicp1-09/sicp1-09.go
+++ sicp1/sicp1-09/sicp1-09.go
@@ -1,1 +1,39 @@
+// sicp_1-09
+
+package main
+
+import "fmt"
+
+func inc(n int) int {
+	return n + 1
+}
+
+func dec(n int) int {
+	return n - 1
+}
+
+// linear recursive process, no tail recursion
+func plusR(a, b int) int {
+	fmt.Println("--", a, b)
+	if a == 0 {
+		return b
+	} else {
+		return inc(plusR(dec(a), b))
+	}
+}
+
+// linear iterative process, with tail recursion
+func plusI(a, b int) int {
+	fmt.Println("--", a, b)
+	if a == 0 {
+		return b
+	} else {
+		return plusI(dec(a), inc(b))
+	}
+}
+
+func main() {
+	fmt.Println(plusI(3, 2))
+	fmt.Println(plusR(3, 2))
+}
 
