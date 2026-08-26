b00140791d72a5de9313991e9c4da7db2a6b5b85
Fedor Andrianov <58761096+fedandr@users.noreply.github.com>
2026-08-26 21:07:00 +0300

    Create sicp1-09.scm

---
+++ sicp1/sicp1-09/sicp1-09.scm
@@ -0,0 +1,25 @@
+;; sicp_1-09
+
+(define (inc n)
+  (+ n 1))
+
+(define (dec n)
+  (- n 1))
+
+;; linear recursive process, no tail recursion
+(define (plus-r a b)
+  (display (list "-->" a b))
+  (newline)
+  (if (= a 0) b (inc (plus-r (dec a) b))))
+
+;; linear iterative process, with tail recursion
+(define (plus-i a b)
+  (display (list "-->" a b))
+  (newline)
+  (if (= a 0) b (plus-i (dec a) (inc b))))
+
+(newline)
+(display (plus-i 3 2))
+(newline)(newline)
+(display (plus-r 3 2))
+(newline)
